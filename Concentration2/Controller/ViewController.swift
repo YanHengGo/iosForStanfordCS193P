09cf3cb499b6b870538d037ac52445d104316372
yanheng <genmanabu@gmail.com>
2020-05-17 19:29:41 +0900

    chapter4  コレクションはStringに変更する

--- Concentration2/Controller/ViewController.swift
+++ Concentration2/Controller/ViewController.swift
@@ -61,12 +61,14 @@ class ViewController: UIViewController {
         }
     }
     
-    private var emojiChoices = ["👺","😱","🙀","😈","🎃","👻","🤡","👄","💀"]
+    //    private var emojiChoices = ["👺","😱","🙀","😈","🎃","👻","🤡","👄","💀"]
+    private var emojiChoices = "👺😱🙀😈🎃👻🤡👄💀"
     private var emoji = [Int:String]()
     
     private func emoji(for card : Card) -> String {
         if emoji[card.identifier] == nil , emojiChoices.count > 0 {
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card.identifier] = String(emojiChoices.remove(at: randomStringIndex))
         }
         return emoji[card.identifier] ?? "?"
     }
